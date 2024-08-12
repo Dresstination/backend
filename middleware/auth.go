e38401c743b6f0be1f6cc5d4e020c33540f71948
Raj Varsani <varasaniraj5@gmail.com>
2024-08-13 01:42:58 +0530

    feat: add authorization

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,42 +1,51 @@
 package middleware
 
 import (
-    "net/http"
-    "strings"
+	"net/http"
+	"strings"
 
-    "github.com/gin-gonic/gin"
-    "log"
+	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 var tokens = map[string]string{
-    "token_for_foo":    "foo",
-    "token_for_austin": "austin",
-    "token_for_lena":   "lena",
-    "token_for_manu":   "manu",
+	"token_for_foo":    "foo",
+	"token_for_austin": "austin",
+	"token_for_lena":   "lena",
+	"token_for_manu":   "manu",
 }
 
 // TokenAuthMiddleware validates Bearer Tokens
 func TokenAuthMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        authHeader := c.GetHeader("Authorization")
-        if authHeader == "" {
-            log.Println("Authorization header missing")
-            c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
-            c.Abort()
-            return
-        }
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			log.Println("Authorization header missing")
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+			c.Abort()
+			return
+		}
 
-        token := strings.TrimPrefix(authHeader, "Bearer ")
-        user, exists := tokens[token]
-        if !exists {
-            log.Printf("Invalid token: %s\n", token)
-            c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-            c.Abort()
-            return
-        }
+		idToken := strings.TrimPrefix(authHeader, "Bearer ")
 
-        // Set the user in the context
-        c.Set("user", user)
-        c.Next()
-    }
+		client, err := app.Auth(ctx.Background())
+		if err != nil {
+			log.Fatalf("error getting Auth client: %v\n", err)
+		}
+
+		token, err := client.VerifyIDToken(ctx, idToken)
+		if err != nil {
+			log.Printf("error verifying ID token: %v\n", err)
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		log.Printf("Verified ID token: %v\n", token)
+
+		// Set the user in the context
+		c.Set("user", token)
+		c.Next()
+	}
 }
